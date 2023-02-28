8a453485b6a679d27d875e0f97d8b9f2adfb5f95
walisked <97894614+walisked@users.noreply.github.com>
2023-02-28 16:31:05 +0100

    Delete new pro directory

--- new pro/main.js
+++
@@ -1,9 +0,0 @@
-const persentYear = new Date().getFullYear(); // get the current year
-
-let yearString 
-for(let year = 2019; year <= persentYear; year++){
-    yearString += year + '';
-}
-
-const yearCountElement = document.getElementById('year');
-yearCountElement.textContent = `${persentYear}`;
